d027b5ad27a04daba2146e7a9408bf34a57c41fb
David <83355091+DalosanESP@users.noreply.github.com>
2022-10-19 00:59:36 +0200

    Update ThreadMostrar.java

--- ThreadMostrar.java
+++ ThreadMostrar.java
@@ -2,26 +2,20 @@ package practica2;
 
 
 public class ThreadMostrar extends Thread{
-		private double [][] resultadoMatriz;
-		private double [] modulo;
+		private double [] resultadoMatriz;
 		
-	public ThreadMostrar(double[][] resultadoMatriz, double[] modulo) {
+	public ThreadMostrar(double[] resultadoMatriz) {
 		this.resultadoMatriz = resultadoMatriz;
-		this.modulo = modulo;
 	}
 		
 	public void run() {
 		for(int i = 0; i < resultadoMatriz.length; i++) {
-			double v = 0;
-			for(int j = 0; j < resultadoMatriz[i].length; j ++) {
-				v = v + resultadoMatriz[i][j];
-			}
-			modulo[i] = v * v;
+			resultadoMatriz[i] = resultadoMatriz[i] * resultadoMatriz[i];
 		}
 		double suma = 0;
-		for(int i = 0; i < modulo.length; i ++) {
-			suma = suma + modulo[i];
-			
+		for(int i = 0; i < resultadoMatriz.length; i ++) {
+			suma = suma + resultadoMatriz[i];
+			System.out.println(suma);
 		}
 		double raiz = Math.sqrt(suma);
 		System.out.println("modulo del vector: " + raiz);
