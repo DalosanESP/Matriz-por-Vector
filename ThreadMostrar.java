39aa51fd63ccbfe0643e791aac9f41fdf4fac79a
David <83355091+DalosanESP@users.noreply.github.com>
2022-10-20 01:40:03 +0200

    Update ThreadMostrar.java

--- ThreadMostrar.java
+++ ThreadMostrar.java
@@ -1,19 +1,21 @@
 package practica2;
 
-
-public class ThreadMostrar extends Thread{
-		private double [] resultadoMatriz;
-		
+public class ThreadMostrar extends Thread {
+	private double[] resultadoMatriz;
+	/*
+	 * Pre:---
+	 * Post:
+	 */
 	public ThreadMostrar(double[] resultadoMatriz) {
 		this.resultadoMatriz = resultadoMatriz;
 	}
-		
+
 	public void run() {
-		for(int i = 0; i < resultadoMatriz.length; i++) {
+		for (int i = 0; i < resultadoMatriz.length; i++) {
 			resultadoMatriz[i] = resultadoMatriz[i] * resultadoMatriz[i];
 		}
 		double suma = 0;
-		for(int i = 0; i < resultadoMatriz.length; i ++) {
+		for (int i = 0; i < resultadoMatriz.length; i++) {
 			suma = suma + resultadoMatriz[i];
 			System.out.println(suma);
 		}
