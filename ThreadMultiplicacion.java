d7fc6fa8cd8ca36a73a5d53733342fa819b6846e
David <83355091+DalosanESP@users.noreply.github.com>
2022-10-24 09:40:07 +0200

    Update ThreadMultiplicacion.java

--- ThreadMultiplicacion.java
+++ ThreadMultiplicacion.java
@@ -1,57 +1,76 @@
-
 package practica2;
 
-public class ThreadMultiplicacion extends Thread{//creamos el metodo constructor
-	private double [][] matriz; //le pasamos la matriz (filas y columnas)
-	private double [] vector; //y el vector por el cual vamos a multiplicar la matriz
-	private int posicion;//un valor que será la posicion, para que los resultados salgan de manera ordenada
-	private double resultado[];
-	
-	
-	
-public ThreadMultiplicacion(double[][] matriz, double[] vector, int posicion, double resultado[] ) {
-	this.matriz = matriz;
-	this.vector = vector;package practica2;
-
-public class ThreadMultiplicacion extends Thread {// creamos el metodo constructor
-	private double[][] matriz; // le pasamos la matriz (filas y columnas)
-	private double[] vector; // y el vector por el cual vamos a multiplicar la matriz
-	private int posicion;// un valor que será la posicion, para que los resultados salgan de manera
-							// ordenada
-	private double resultado[];
+public class ThreadMultiplicacion extends Thread {
+	long start = System.currentTimeMillis();
+	private int posicionInicial;
+	private int posicionFinal;
+	private DatosCompartidos dc;
+	private long tiempo;
 
 	/*
 	 * Pre:---
-	 * Post:
+	 * Post: Constructor de ThreadMultiplicacion
 	 */
-	public ThreadMultiplicacion(double[][] matriz, double[] vector, int posicion, double resultado[]) {
-		this.matriz = matriz;
-		this.vector = vector;
-		this.posicion = posicion;
-		this.resultado = resultado;
+	public ThreadMultiplicacion( DatosCompartidos dc, int posicionInicial, int posicionFinal) {
+		this.dc = dc;
+		this.posicionInicial = posicionInicial;
+		this.posicionFinal = posicionFinal;
 	}
 
+	/*
+	 * Pre:---
+	 * Post: Multiplicacion de la matriz por el vector
+	 */
 	public void run() {
-		// multiplicamos la matriz por el vector
-		for (int i = posicion; i < posicion + 1; i++) {
-			for (int j = 0; j < matriz[i].length; j++) {
-				resultado[i] = matriz[i][j] * vector[j] + resultado[i];
+		for (int i = posicionInicial; i < posicionFinal; i++) {
+			for (int j = 0; j < dc.matriz[i].length; j++) {
+				dc.resultado[i] = dc.matriz[i][j] * dc.vector[j] + dc.resultado[i];
 			}
-			i = i + 1;
 		}
+		long end = System.currentTimeMillis();
+		long elapsedTime = end - start;
+		tiempo = elapsedTime;
 	}
-}
-	this.posicion = posicion;
-	this.resultado = resultado;
-}
 	
-public void run() {
-	//multiplicamos la matriz por el vector
-	for(int i = posicion; i < posicion + 1; i++){
-			for(int j = 0; j < matriz[i].length; j ++) {
-				resultado[i] = matriz[i][j] * vector[j] + resultado[i];
-			}
-			i = i +1;
-		}
+	//Getters and Setters
+	public long getStart() {
+		return start;
 	}
+
+	public void setStart(long start) {
+		this.start = start;
+	}
+
+	public int getPosicionInicial() {
+		return posicionInicial;
+	}
+
+	public void setPosicionInicial(int posicionInicial) {
+		this.posicionInicial = posicionInicial;
+	}
+
+	public int getPosicionFinal() {
+		return posicionFinal;
+	}
+
+	public void setPosicionFinal(int posicionFinal) {
+		this.posicionFinal = posicionFinal;
+	}
+
+	public DatosCompartidos getDc() {
+		return dc;
+	}
+	
+	public void setDc(DatosCompartidos dc) {
+		this.dc = dc;
+	}
+
+	public long getTiempo() {
+		return tiempo;
+	}
+
+	public void setTiempo(long tiempo) {
+		this.tiempo = tiempo;
+	}
+
 }
